6133e4f55957e530c81fafd737fdca5e3a686311
ytorres1 <ytorres@terpmail.umd.edu>
2023-02-19 18:27:01 -0500

    Final Changes

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -19,6 +19,10 @@ const slidesArray = Array.from(slides);
 const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
+  slidesArray.forEach((slide) => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden');
+  });
   // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
   // loop through all the slides in your slideArray
   // and remove the 'visible' class from each classList
@@ -26,7 +30,12 @@ function updateSlidePosition() {
 
   // outside your .forEach,
   // add a 'visible' class to the slide at the current slidePosition in slides
-}
+  console.log(slidePosition)
+  slides[slidePosition].classList.add("visible");
+};
+
+
+
 
 function moveToNextSlide() {
   /*
@@ -35,6 +44,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+ if(slidePosition === totalSlides - 1){
+    slidePosition = 0;
+ } else {
+    slidePosition += 1;
+ }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -45,6 +59,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+ if(slidePosition === 0){
+  slidePosition = totalSlides - 1;
+ } else {
+  slidePosition = slidePosition - 1;
+ }
   updateSlidePosition();
 }
 
